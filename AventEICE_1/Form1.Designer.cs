55ec6853f0175402a402be48cd40401a9bda6c9b
MMeinert <m.meinertzhagen@hainaut-promsoc.be>
2020-09-08 19:08:28 +0200

    Rajout d'un label de test

--- AventEICE_1/Form1.Designer.cs
+++ AventEICE_1/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1_git_test = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1_git_test
+            // 
+            this.label1_git_test.AutoSize = true;
+            this.label1_git_test.Location = new System.Drawing.Point(62, 48);
+            this.label1_git_test.Name = "label1_git_test";
+            this.label1_git_test.Size = new System.Drawing.Size(68, 13);
+            this.label1_git_test.TabIndex = 0;
+            this.label1_git_test.Text = "Label de test";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1_git_test);
             this.Name = "Form1";
             this.Text = "Les aventuriers de l\'EICE";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1_git_test;
     }
 }
 
--- AventEICE_1/Form1.cs
+++ AventEICE_1/Form1.cs
@@ -16,5 +16,10 @@ namespace AventEICE_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
